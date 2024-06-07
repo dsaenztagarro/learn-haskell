21f5f87e4859613c8e17f3c9c1158778805a470f
David Sáenz <dsaenz@bebanjo.com>
2024-06-07 20:45:41 +0200

    Add project skeleton with specs

---
+++ test/Extensions/FunctionalDependenciesSpec.hs
@@ -0,0 +1,48 @@
+module Extensions.FunctionalDependenciesSpec (spec) where
+
+import Test.Hspec
+import Extensions.FunctionalDependencies
+
+spec :: Spec
+spec = do
+  describe "FunctionalDependencies" $ do
+    it "works ListDirectory shell command" $ do
+      let dir = "/Users/dsaenz/Code/effective-haskell/test/dummy/src/"
+      result <- runShellCommand (ListDirectory dir)
+      result `shouldBe` ["FileA.hs", "FileB.hs", "FileC.hs"]
+
+    it "works Grep shell command" $ do
+      let match = "module"
+          files = [
+            "/Users/dsaenz/Code/effective-haskell/test/dummy/src/FileA.hs",
+            "/Users/dsaenz/Code/effective-haskell/test/dummy/src/FileB.hs",
+            "/Users/dsaenz/Code/effective-haskell/test/dummy/src/FileC.hs"]
+      result <- runShellCommand (Grep match files)
+      result `shouldBe` [
+          GrepMatch {
+            grepMatchingFileName = "/Users/dsaenz/Code/effective-haskell/test/dummy/src/FileA.hs",
+            grepMatchingLineNumber = 1,
+            grepMatchingLineContents = "module MyFile where"
+          },
+          GrepMatch {
+            grepMatchingFileName = "/Users/dsaenz/Code/effective-haskell/test/dummy/src/FileC.hs",
+            grepMatchingLineNumber = 1,
+            grepMatchingLineContents = "module FileC where"
+          }
+        ]
+
+    it "works grepFilesInDirectory shell command" $ do
+      let dir = "/Users/dsaenz/Code/effective-haskell/test/dummy/src"
+      result <- runShellCommand (grepFilesInDirectory "module" dir)
+      result `shouldBe` [
+          GrepMatch {
+            grepMatchingFileName = "/Users/dsaenz/Code/effective-haskell/test/dummy/src/FileA.hs",
+            grepMatchingLineNumber = 1,
+            grepMatchingLineContents = "module MyFile where"
+          },
+          GrepMatch {
+            grepMatchingFileName = "/Users/dsaenz/Code/effective-haskell/test/dummy/src/FileC.hs",
+            grepMatchingLineNumber = 1,
+            grepMatchingLineContents = "module FileC where"
+          }
+        ]
