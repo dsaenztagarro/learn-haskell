2452b8c6f460456f5383f49b77a27221655ff1f3
David Sáenz <dsaenz@bebanjo.com>
2024-06-14 16:58:24 +0200

    Use spec helper with associated type families

--- test/GHCExts/AssociatedTypeFamiliesSpec.hs
+++ test/GHCExts/AssociatedTypeFamiliesSpec.hs
@@ -2,51 +2,48 @@ module GHCExts.AssociatedTypeFamiliesSpec (spec) where
 
 import Test.Hspec
 import GHCExts.AssociatedTypeFamilies
-
--- TODO: extract hook to create temp dir and files with content
--- import System.Directory (getTemporaryDirectory, makeAbsolute)
+import System.FilePath ((</>))
+import GHCExts.Helper (withTestDir)
 
 spec :: Spec
 spec = do
-  -- describe "AssociatedTypeFamilies" $ around_ withTempDir do
-  describe "AssociatedTypeFamilies" $ do
-    it "works ListDirectory shell command" $ do
-      let dir = "/Users/dsaenz/Code/effective-haskell/test/dummy/src/"
-      result <- runShellCommand (ListDirectory dir)
-      result `shouldBe` ["FileA.hs", "FileB.hs", "FileC.hs"]
+  describe "AssociatedTypeFamilies" $
+    around withTestDir $ do
+      it "works ListDirectory shell command" $ \testDir -> do
+        result <- runShellCommand (ListDirectory testDir)
+        result `shouldBe` ["FileA.hs", "FileB.hs", "FileC.hs"]
 
-    it "works Grep shell command" $ do
-      let match = "module"
-          files = [
-            "/Users/dsaenz/Code/effective-haskell/test/dummy/src/FileA.hs",
-            "/Users/dsaenz/Code/effective-haskell/test/dummy/src/FileB.hs",
-            "/Users/dsaenz/Code/effective-haskell/test/dummy/src/FileC.hs"]
-      result <- runShellCommand (Grep match files)
-      result `shouldBe` [
-          GrepMatch {
-            grepMatchingFileName = "/Users/dsaenz/Code/effective-haskell/test/dummy/src/FileA.hs",
-            grepMatchingLineNumber = 1,
-            grepMatchingLineContents = "module MyFile where"
-          },
-          GrepMatch {
-            grepMatchingFileName = "/Users/dsaenz/Code/effective-haskell/test/dummy/src/FileC.hs",
-            grepMatchingLineNumber = 1,
-            grepMatchingLineContents = "module FileC where"
-          }
-        ]
+      it "works Grep shell command" $ \testDir -> do
+        let match = "module"
+            files = [ testDir </> "FileA.hs"
+                    , testDir </> "FileB.hs"
+                    , testDir </> "FileC.hs"
+                    ]
+        result <- runShellCommand (Grep match files)
+        result `shouldBe` [
+            GrepMatch {
+              grepMatchingFileName = testDir </> "FileA.hs",
+              grepMatchingLineNumber = 1,
+              grepMatchingLineContents = "module FileA where"
+            },
+            GrepMatch {
+              grepMatchingFileName = testDir </> "FileC.hs",
+              grepMatchingLineNumber = 2,
+              grepMatchingLineContents = "module FileC where"
+            }
+          ]
 
-    it "works grepFilesInDirectory shell command" $ do
-      let dir = "/Users/dsaenz/Code/effective-haskell/test/dummy/src"
-      result <- runShellCommand (grepFilesInDirectory "module" dir)
-      result `shouldBe` [
-          GrepMatch {
-            grepMatchingFileName = "/Users/dsaenz/Code/effective-haskell/test/dummy/src/FileA.hs",
-            grepMatchingLineNumber = 1,
-            grepMatchingLineContents = "module MyFile where"
-          },
-          GrepMatch {
-            grepMatchingFileName = "/Users/dsaenz/Code/effective-haskell/test/dummy/src/FileC.hs",
-            grepMatchingLineNumber = 1,
-            grepMatchingLineContents = "module FileC where"
-          }
-        ]
+      it "works grepFilesInDirectory shell command" $ \testDir -> do
+        result <- runShellCommand (grepFilesInDirectory "module" testDir)
+        result `shouldBe` [
+            GrepMatch {
+              grepMatchingFileName = testDir </> "FileA.hs",
+              grepMatchingLineNumber = 1,
+              grepMatchingLineContents = "module FileA where"
+            },
+            GrepMatch {
+              grepMatchingFileName = testDir </> "FileC.hs",
+              grepMatchingLineNumber = 2,
+              grepMatchingLineContents = "module FileC where"
+            }
+          ]
