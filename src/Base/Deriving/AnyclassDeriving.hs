8159bc9c58e67c878c9ad5a05a9dd496543a6890
David Sáenz <dsaenz@bebanjo.com>
2024-07-12 13:52:21 +0200

    Review DefaultSignatures

--- src/Base/Deriving/AnyclassDeriving.hs
+++ src/Base/Deriving/AnyclassDeriving.hs
@@ -2,12 +2,20 @@
 {-# LANGUAGE DeriveAnyClass #-}
 module Base.Deriving.AnyclassDeriving where
 
+{-
+Haskell 98 allows you to define a default implementation when declaring a class:
+
 class Redacted a where
   redacted :: a -> String
-  {-
-  `DefaultSignatures` extension allow us to add a type signature to the default
-  implementation of a function in a type class.
-  -}
+  redacted = "Untitled"
+
+The type of the `redacted` method is `a -> String`, and this is also the type of
+the default method. You can lift this restriction and give another type to the
+default method using the flag -XDefaultSignatures.
+-}
+
+class Redacted a where
+  redacted :: a -> String
   default redacted :: Show a => a -> String
   redacted = show
 
