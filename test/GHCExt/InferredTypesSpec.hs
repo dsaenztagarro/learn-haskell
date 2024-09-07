e3f4ce50df621e87cc2b5e8c12cd7723c418ef5a
David Saenz <dsaenz@Davids-MacBook-Pro.local>
2024-09-07 12:52:47 +0200

    Add comment on inferred types specs

--- test/GHCExt/InferredTypesSpec.hs
+++ test/GHCExt/InferredTypesSpec.hs
@@ -7,13 +7,27 @@ import GHCExt.InferredTypes
 
 spec :: Spec
 spec = do
-  -- fromIntegral :: (Integral a, Num b) => a -> b
+
+  {-
+  Example where all type variables are specified types
+
+  :t fromIntegral
+
+  fromIntegral :: (Integral a, Num b) => a -> b
+
+  :set -fprint-explicit-foralls
+  :t fromIntegral
+
+  fromIntegral :: forall a b. (Integral a, Num b) => a -> b
+  -}
   describe "fromIntegral" $ do
     it "allows type application in all specified types" $ do
       fromIntegral @Int @Double (3 :: Int) `shouldBe` 3.0
 
     it "allows type application on output while keeping input polymorphic" $ do
       fromIntegral @_ @Float (3 :: Word) `shouldBe` 3.0
+
+  -- Examples with both inferred and specified types
 
   describe "convertViaInt" $ do
     it "allows type application on output specified type, skipping inferred input type" $ do
